2c534e05d78caf8c5a5db4b2ec33f7b5ab6bb4af
Denis Zhurov <den_fobos@mail.ru>
2021-03-17 18:12:52 +0300

    updated /08_if_else/index.html, /08_if_else/script.js, /08_if_else/style.css - was made game 'guessing number'

--- bjs/07_Number_and_string/script_2.js
+++ bjs/07_Number_and_string/script_2.js
@@ -19,7 +19,7 @@ function checkPressedButtons() {
         }));
     document.addEventListener('keydown', (event) =>{
         let keyboardKey = event.key;
-        if(keyboardKey === 'Enter'){
+        if (keyboardKey === 'Enter') {
             event.preventDefault();
         }
         keyBoardHandler(keyboardKey);
@@ -30,78 +30,57 @@ function checkPressedButtons() {
 //output digits to the screen
 function keyOutput(kId)
 {
-    if ( kId === 'btn_0' )
-    {
+    if (kId === 'btn_0' ) {
         displayVar = displayVar.toString();
         displayVar += '0';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    } 
-    else if ( kId === 'btn_1' )
-    {
+    } else if (kId === 'btn_1' ) {
         displayVar = displayVar.toString();
         displayVar += '1';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    } 
-    else if ( kId === 'btn_2' )
-    {
+    } else if (kId === 'btn_2' ) {
         displayVar = displayVar.toString();
         displayVar += '2';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    } 
-    else if ( kId === 'btn_3' )
-    {
+    } else if (kId === 'btn_3' ) {
         displayVar = displayVar.toString();
         displayVar += '3';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    } 
-    else if ( kId === 'btn_4' )
-    {
+    } else if (kId === 'btn_4' ) {
         displayVar = displayVar.toString();
         displayVar += '4';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    } 
-    else if ( kId === 'btn_5' )
-    {
+    } else if (kId === 'btn_5' ) {
         displayVar = displayVar.toString();
         displayVar += '5';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    } 
-    else if ( kId === 'btn_6' )
-    {
+    } else if (kId === 'btn_6' ) {
         displayVar = displayVar.toString();
         displayVar += '6';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    } 
-    else if ( kId === 'btn_7' )
-    {
+    } else if (kId === 'btn_7' ) {
         displayVar = displayVar.toString();
         displayVar += '7';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    }
-    else if ( kId === 'btn_8' )
-    {
+    } else if (kId === 'btn_8' ) {
         displayVar = displayVar.toString();
         displayVar += '8';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    } 
-    else if ( kId === 'btn_9' )
-    {
+    } else if (kId === 'btn_9' ) {
         displayVar = displayVar.toString();
         displayVar += '9';
         displayVar = Number(displayVar);
         inputWindow.textContent = displayVar;
-    }
-    else if ( kId === 'btn_comma' )
-    {
+    } else if (kId === 'btn_comma' ) {
         displayVar = displayVar.toString();
         displayVar += '.';
         //displayVar = parseFloat(displayVar);
@@ -112,48 +91,31 @@ function keyOutput(kId)
 //Make binary operation on display content. Input should be pressed button of operation.
 function unaryOperator(kId) 
 {   displayVar = Number(displayVar);
-    if(kId === 'btn_plusmn')
-    {
+    if(kId === 'btn_plusmn') {
         displayVar = - displayVar
         inputWindow.textContent = displayVar;
-    }
-    else if(kId === 'btn_sqrt')
-    {
+    } else if(kId === 'btn_sqrt') {
         displayVar = Math.sqrt(displayVar);
         inputWindow.textContent = displayVar;
-    }
-    else if(kId === 'btn_square')
-    {
+    } else if(kId === 'btn_square') {
         displayVar = displayVar ** 2;
         inputWindow.textContent = displayVar;
-    }
-    else if(kId === 'btn_reciprocal')
-    {
+    } else if(kId === 'btn_reciprocal') {
         displayVar = 1 / displayVar;
         inputWindow.textContent = displayVar;
-    }
-    else if(kId === 'btn_lg')
-    {
+    } else if(kId === 'btn_lg') {
         displayVar = Math.log10(displayVar);
         inputWindow.textContent = displayVar;
-    }
-    else if(kId === 'btn_cbrt')
-    {
+    } else if(kId === 'btn_cbrt') {
         displayVar = Math.cbrt(displayVar);
         inputWindow.textContent = displayVar;
-    }
-    else if(kId === 'btn_sin')
-    {
+    } else if(kId === 'btn_sin') {
         displayVar = Math.sin(displayVar * Math.PI / 180);
         inputWindow.textContent = displayVar;
-    }
-    else if(kId === 'btn_cos')
-    {
+    } else if(kId === 'btn_cos') {
         displayVar = Math.cos(displayVar * Math.PI / 180);
         inputWindow.textContent = displayVar;
-    }
-    else if(kId === 'btn_ln')
-    {
+    } else if(kId === 'btn_ln') {
         displayVar = Math.log(displayVar);
         inputWindow.textContent = displayVar;
     }
@@ -166,26 +128,19 @@ function unaryOperator(kId)
 function binaryOperator(kId)
 {
     displayVar = Number(displayVar);
-    if(kId === 'sum')
-    {
+    if (kId === 'sum') {
         displayVar = firstOperand + displayVar;
         inputWindow.textContent = displayVar;
         firstOperand = displayVar;
-    }
-    else if(kId === 'sub')
-    {
+    } else if (kId === 'sub') {
         displayVar = firstOperand - displayVar;
         inputWindow.textContent = displayVar;
         firstOperand = displayVar;
-    }
-    else if(kId === 'mult')
-    {
+    } else if (kId === 'mult') {
         displayVar = firstOperand * displayVar;
         inputWindow.textContent = displayVar;
         firstOperand = displayVar;
-    }
-    else if(kId === 'div')
-    {
+    } else if (kId === 'div') {
         displayVar = firstOperand / displayVar;
         inputWindow.textContent = displayVar;
         firstOperand = displayVar;
@@ -196,17 +151,29 @@ function binaryOperator(kId)
 //Handle key input and write meaning of operation to variable operation
 function operationDeclaration(kId)
 {   
-    if(kId === 'btn_sum'){operation = 'sum'; oper = '+';}
-    else if(kId === 'btn_sub'){operation = 'sub';}
-    else if(kId === 'btn_multiply'){operation = 'mult';}
-    else if(kId === 'btn_division'){operation = 'div';}
+    if(kId === 'btn_sum') {
+        operation = 'sum';
+    } else if(kId === 'btn_sub') {
+        operation = 'sub';
+    } else if(kId === 'btn_multiply') {
+        operation = 'mult';
+    } else if(kId === 'btn_division') {
+        operation = 'div';
+    }
 }
 function operationExplanation(kId)
 {   let oper;
-    if(kId === 'btn_sum'){oper = '+';}
-    else if(kId === 'btn_sub'){oper = '-';}
-    else if(kId === 'btn_multiply'){oper = '*';}
-    else if(kId === 'btn_division'){oper = '/';}
+    if (kId === 'btn_sum') {
+        oper = '+';
+    } else if (kId === 'btn_sub') {
+        oper = '-';
+    }
+    else if (kId === 'btn_multiply') {
+        oper = '*';
+    }
+    else if (kId === 'btn_division') {
+        oper = '/';
+    }
     return oper;
 }
 
@@ -214,190 +181,126 @@ function operationExplanation(kId)
 function flowOutput(kId, kClass, lastButtonClass)
 {
     let oper = operationExplanation(kId);
-    if (kId === 'btn_result')
-    {
+    if (kId === 'btn_result') {
         flowOutputWindow.textContent = '';
-    }
-    else if (kClass.includes('binary') && lastButtonClass !== 'unary')
-    {
+    } else if ( kClass.includes('binary') && lastButtonClass !== 'unary' ) {
         flowOutputWindow.textContent += `${displayVar} ${oper} `;
-    }
-    else if (kClass.includes('binary'))
-    {
+    } else if ( kClass.includes('binary') ) {
         flowOutputWindow.textContent += ` ${oper} `;
     }
     //output unary operations
-    else if (kClass.includes('unary'))
-    {
-        if(kId === 'btn_sqrt')
-        {
+    else if ( kClass.includes('unary') ) {
+        if (kId === 'btn_sqrt') {
             flowOutputWindow.textContent += `sqrt(${displayVar}) `;
-        }
-        else if(kId === 'btn_square')
-        {
+        } else if(kId === 'btn_square') {
             flowOutputWindow.textContent += `${displayVar}^2 `;
-        }
-        else if(kId === 'btn_reciprocal')
-        {
+        } else if(kId === 'btn_reciprocal') {
             flowOutputWindow.textContent += `1/${displayVar} `;
-        }
-        else if(kId === 'btn_lg')
-        {
+        } else if(kId === 'btn_lg') {
             flowOutputWindow.textContent += `lg(${displayVar}) `;
-        }
-        else if(kId === 'btn_cbrt')
-        {
+        } else if(kId === 'btn_cbrt') {
             flowOutputWindow.textContent += `cbrt(${displayVar}) `;
-        }
-        else if(kId === 'btn_sin')
-        {
+        } else if(kId === 'btn_sin') {
             flowOutputWindow.textContent += `sin(${displayVar}) `;
-        }
-        else if(kId === 'btn_cos')
-        {
+        } else if(kId === 'btn_cos') {
             flowOutputWindow.textContent += `cos(${displayVar}) `;
-        }
-        else if(kId === 'btn_ln')
-        {
+        } else if(kId === 'btn_ln') {
             flowOutputWindow.textContent += `ln(${displayVar}) `;
         }
-    }
-    else if (kId === "btn_clr")
-    {
+    } else if (kId === "btn_clr") {
         flowOutputWindow.textContent = '';
     }
-    
-
 }
 
 //handle keyboard input in addition to mouse input from page
-function keyBoardHandler(keyboardKey)
-{
+function keyBoardHandler(keyboardKey) {
         // operation keys 
-        if(keyboardKey === '+')
-        {
+        if (keyboardKey === '+') {
             keyId = 'btn_sum';
             keyClass = 'binary';
-        }
-        else if(keyboardKey === '-')
-        {
+        } else if (keyboardKey === '-') {
             keyId = 'btn_sub';
             keyClass = 'binary';
-        }
-        else if(keyboardKey === '*')
-        {
+        } else if (keyboardKey === '*') {
             keyId = 'btn_multiply';
             keyClass = 'binary';
-        }
-        else if(keyboardKey === '/')
-        {
+        } else if (keyboardKey === '/') {
             keyId = 'btn_division';
             keyClass = 'binary';
-        }
-        else if(keyboardKey === 'Enter')
-        {
+        } else if (keyboardKey === 'Enter') {
             keyId = 'btn_result';
             keyClass = 'binary';
         }
         //number keys
-        else if(keyboardKey === '0')
-        {
+        else if (keyboardKey === '0') {
             keyId = 'btn_0';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '1')
-        {
+        } else if (keyboardKey === '1') {
             keyId = 'btn_1';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '2')
-        {
+        } else if (keyboardKey === '2') {
             keyId = 'btn_2';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '3')
-        {
+        } else if (keyboardKey === '3') {
             keyId = 'btn_3';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '4')
-        {
+        } else if (keyboardKey === '4') {
             keyId = 'btn_4';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '5')
-        {
+        } else if (keyboardKey === '5') {
             keyId = 'btn_5';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '6')
-        {
+        } else if (keyboardKey === '6') {
             keyId = 'btn_6';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '7')
-        {
+        } else if (keyboardKey === '7') {
             keyId = 'btn_7';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '8')
-        {
+        } else if (keyboardKey === '8') {
             keyId = 'btn_8';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '9')
-        {
+        } else if (keyboardKey === '9') {
             keyId = 'btn_9';
             keyClass = 'nums';
-        }
-        else if(keyboardKey === '.')
-        {
+        } else if (keyboardKey === '.') {
             keyId = 'btn_comma';
             keyClass = 'nums';
         }
         //service keys
-        else if(keyboardKey === 'Escape')
-        {
+        else if (keyboardKey === 'Escape') {
             keyId = 'btn_clr';
             keyClass = '';
-        }
-        else if(keyboardKey === 'Delete')
-        {
+        } else if (keyboardKey === 'Delete') {
             keyId = 'btn_clr';
             keyClass = 'service';
-        }
-        else if(keyboardKey === 'Enter')
-        {   
+        } else if (keyboardKey === 'Enter') {   
             keyId = 'btn_result';
             keyClass = 'binary';
         }
+
         calcLogic(keyId, keyClass);
 }
 
 function calcLogic(kId, kClass)
 {
-    if(kId === 'btn_clr')
-    {
+    if(kId === 'btn_clr') {
         firstOperand = null;
         operation = null;
         lastButtonClass = null;
         inputWindow.textContent = 0;
         displayVar = '';
     }
-    if(kId === 'btn_ce')
-    {
+
+    if(kId === 'btn_ce') {
         inputWindow.textContent = 0;
         displayVar = '';
     }
     
     flowOutput(kId, kClass, lastButtonClass);
 
-    if(firstOperand !== null)
-    {
-        if(kClass.includes('nums'))
-        {
-            if(lastButtonClass === 'nums')
-            {
+    if(firstOperand !== null) {
+        if(kClass.includes('nums')) {
+            if(lastButtonClass === 'nums') {
                 keyOutput(kId);
             }
             else if(lastButtonClass === 'binary' || lastButtonClass === 'unary' || lastButtonClass === 'service')
@@ -449,21 +352,20 @@ function calcLogic(kId, kClass)
                 }
             }
         }
-    }
-    else  //when fist operand = null
-    {
-        if(inputWindow.textContent === '0')
+    } else {
+        //when first operand === null
+        if (inputWindow.textContent === '0')
         {
-            if(kClass.includes('nums'))
+            if (kClass.includes('nums'))
             {
                 displayVar = '';
                 inputWindow.textContent = displayVar;
                 keyOutput(kId);
             }
         }
-        else if(kClass.includes('nums'))
+        else if (kClass.includes('nums'))
         {       
-            if(lastButtonClass === 'unary')
+            if (lastButtonClass === 'unary')
             {   
                 displayVar = '';
                 inputWindow.textContent = displayVar;
@@ -487,10 +389,7 @@ function calcLogic(kId, kClass)
                 operationDeclaration(kId);
             }
         }
-        else
-        {
-            //for future use
-        }
+        
     }
    
     if (kClass.includes('nums'))
--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,20 +1,194 @@
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
-alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
-let answerNumber  = Math.floor((minValue + maxValue) / 2);
-let orderNumber = 1;
-let gameRun = true;
-
+let minValue;// = parseInt(prompt('Минимальное знание числа для игры')) || 0;
+let maxValue;// = parseInt(prompt('Максимальное знание числа для игры')) || 100;
+const popup = document.querySelector('#minmax');
+const startButton = document.querySelector('.start');
+const setInterval = document.querySelector('#set_interval');
+const messageConfirmation = document.querySelector('#message');
+const alertWindow = document.querySelector('#alert')
 const orderNumberField = document.getElementById('orderNumberField');
 const answerField = document.getElementById('answerField');
 
-orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber }?`;
+let answerNumber;
+let orderNumber;
+let gameRun;
 
-document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
+
+
+startButton.addEventListener('click', () => {
+    popup.showModal();
+    startButton.style.display =  'none';
+})
+
+function textolization (number) {
+    let text = '';
+    let ones = 0;
+    let tens = 0;
+    let hundreds = 0;
+    let sign = '';
+    let onesText = '';
+    let tensText = '';
+    let hundredsText = '';
+
+    function onesToText(ones) {
+        onesText = '';
+        ones = Math.abs(ones);
+        if (ones === 1) {
+            onesText = 'один';
+        } else if (ones === 2) {
+            onesText = 'два';
+        } else if (ones === 3) {
+            onesText = 'три';
+        } else if (ones === 4) {
+            onesText = 'четыре';
+        } else if (ones === 5) {
+            onesText = 'пять';
+        } else if (ones === 6) {
+            onesText = 'шесть';
+        } else if (ones === 7) {
+            onesText = 'семь';
+        } else if (ones === 8) {
+            onesText = 'восемь';
+        } else if (ones === 9) {
+            onesText = 'девять';
+        } else if (ones === 10) {
+            onesText = 'десять';
+        } else if (ones === 11) {
+            onesText = 'одиннадцать';
+        } else if (ones === 12) {
+            onesText = 'двенадцать';
+        } else if (ones === 13) {
+            onesText = 'тринадцать';
+        } else if (ones === 14) {
+            onesText = 'четырнадцать';
+        } else if (ones === 15) {
+            onesText = 'пятнадцать';
+        } else if (ones === 16) {
+            onesText = 'шестнадцать';
+        } else if (ones === 17) {
+            onesText = 'семнадцать';
+        } else if (ones === 18) {
+            onesText = 'восемнадцать';
+        } else if (ones === 19) {
+            onesText = 'девятнадцать';
+        } else if (ones === 20) {
+            onesText = 'двадцать';
+        } else if (ones === 0) {
+            onesText = '0';
+        }
+    }
+    function tensToText(tens) {
+        tensText = '';
+        tens = Math.abs(tens);
+        if (tens === 2) {
+            tensText = 'двадцать';
+        } else if (tens === 3) {
+            tensText = 'тридцать';
+        } else if (tens === 4) {
+            tensText = 'сорок';
+        } else if (tens === 5) {
+            tensText = 'пятьдесят';
+        } else if (tens === 6) {
+            tensText = 'шестьдесят';
+        } else if (tens === 7) {
+            tensText = 'семьдесят';
+        } else if (tens === 8) {
+            tensText = 'восемьдесят';
+        } else if (tens === 9) {
+            tensText = 'девяносто';
+        }
+        return tensText;
+    }
+    function hundredsToText(hudreds) {
+        hundredsText = '';
+        hundreds = Math.abs(hundreds);
+        if (hundreds === 1) {
+            hundredsText = 'сто';
+        } else if (hundreds === 2) {
+            hundredsText = 'двести';
+        } else if (hundreds === 3) {
+            hundredsText = 'триста';
+        } else if (hundreds === 4) {
+            hundredsText = 'четыреста';
+        } else if (hundreds === 5) {
+            hundredsText = 'пятьсот';
+        } else if (hundreds === 6) {
+            hundredsText = 'шестьсот';
+        } else if (hundreds === 7) {
+            hundredsText = 'семьсот';
+        } else if (hundreds === 8) {
+            hundredsText = 'восемьсот';
+        } else if (hundreds === 9) {
+            hundredsText = 'девятьсот';
+        }
+    }
+
+    if (number < 0) sign = 'минус';
+   
+    if (Math.abs(number) <= 20) {
+        onesToText(number);
+    } else {
+        if (Math.abs(number % 100) <= 20) {
+            ones = Math.abs(number % 100);
+            hundreds = Math.abs( (number - number % 100) / 100 );
+            onesToText(ones);
+            hundredsToText(hundreds);
+        } else {
+        ones = Math.abs(number % 100 % 10);
+        tens = Math.abs( (number - number % 100 % 10) % 100 / 10 );
+        hundreds = Math.abs( (number - number % 100) / 100 );
+        onesToText(ones);
+        tensToText(tens);
+        hundredsToText(hundreds);
+        }
+    }
+
+    text = `${sign} ${hundredsText} ${tensText} ${onesText}`;
+    if (text.length > 20) {
+        text = number;
+    }
+    return text;
+}   
+
+setInterval.addEventListener('click', (event) => {
+    event.preventDefault();
+    minValue = parseInt(document.querySelector('#min').value) || 0;
+    maxValue = parseInt(document.querySelector('#max').value) || 100;
+
+    if(minValue > maxValue) {
+        [minValue, maxValue] = [maxValue, minValue]
+    }
+    (minValue < -999 ) ? minValue = -999 : minValue = minValue;
+    (minValue > 999 ) ? minValue = 997 : minValue = minValue;
+    (maxValue > 999) ? maxValue = 999 : maxValue = maxValue;
+    (maxValue < -999) ? maxValue = -997 : maxValue = maxValue;
+    messageConfirmation.textContent = `А теперь загадайте любое целое число от ${minValue} до ${maxValue}, а я угадаю его с трех нот!`
+    //alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+
+    answerNumber  = Math.floor((minValue + maxValue) / 2);
+    orderNumber = 1;
+    gameRun = true;
+            
+    popup.close();
+    alertWindow.showModal();
+    
+})
+
+document.querySelector('#confirm').addEventListener('click', () => {
+    document.querySelector('.container').style.display = 'block';
+    orderNumberField.innerText = orderNumber;
+    answerField.innerText = `Вы загадали число ${textolization (answerNumber)}?`;
+
+    alertWindow.close();
+
+})
+
+document.getElementById('btnRetry').addEventListener('click',  (event) => {
+    
+    document.querySelector('#min').value = '';
+    document.querySelector('#max').value = '';
+    popup.showModal();
+    
+    
 })
 
 document.getElementById('btnOver').addEventListener('click', function () {
@@ -23,7 +197,7 @@ document.getElementById('btnOver').addEventListener('click', function () {
             const phraseRandom = Math.round( Math.random());
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
+                `Я сдаюсь..\n\u{1F494}`;
 
             answerField.innerText = answerPhrase;
             gameRun = false;
@@ -32,14 +206,69 @@ document.getElementById('btnOver').addEventListener('click', function () {
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+
+            switch(Math.round( Math.random()*5)) {
+             case 1: answerField.innerText = `Может быть это ${textolization (answerNumber)}?`;
+                break;
+             case 2: answerField.innerText = `Хмм... Это ${textolization (answerNumber)}?`;
+                break;
+             case 3: answerField.innerText = `Ну, может это число ${textolization (answerNumber)}?`;
+                break;      
+             case 4: answerField.innerText = `Готов сделать ставку, что это ${textolization (answerNumber)}!`;
+                break;      
+             case 5: answerField.innerText = `А вот это ${textolization (answerNumber)}?`;
+                break; 
+                default: answerField.innerText = `Вы загадали ${textolization (answerNumber)}?`;   
+            }
+           // answerField.innerText = askPhrase;
+        }
+    }
+})
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue) {
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber - 1;
+            answerNumber  = Math.ceil((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            switch ( Math.round( Math.random()*5) )
+            {case 1: answerField.innerText = `Может быть это ${textolization (answerNumber)}?`;
+                break;
+             case 2: answerField.innerText = `Наверняка это ${textolization (answerNumber)}?`;
+                break;
+             case 3: answerField.innerText = `Ну это точно ${textolization (answerNumber)}!`;
+                break;      
+             case 4: answerField.innerText = `Готов сделать ставку, что это ${textolization (answerNumber)}!`;
+                break;
+             case 5: answerField.innerText = `Последняя попытка... Это ${textolization (answerNumber)}?`;
+                break;      
+                default: answerField.innerText = `Вы загадали ${textolization (answerNumber)}?`;
+            }
+            
         }
     }
 })
 
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+        switch( Math.round( Math.random()*3) ) {
+            case 1: answerField.innerText = `Eeeeee!\n\u{1F525}`;
+                break;
+            case 2: answerField.innerText = `Ура!!! \n\u{1F389}`;
+                break;
+            case 3: answerField.innerText = `Я угадал! Я угадал! \n\u{2757}`;
+                break;
+            default: answerField.innerText = `Я всегда угадываю\n\u{1F60E}`;
+            }
+        
         gameRun = false;
     }
 })

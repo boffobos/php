ae2c4f79ebfba7df52e90d1cc6a7b3d90a98771a
Denis Zhurov <den_fobos@mail.ru>
2021-04-05 23:22:59 +0300

    Updated index.html, init.js, personGenerator.js. Realised father name, birthDate.

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -6,6 +6,9 @@ window.onload = function()
     document.getElementById('fatherNameOutput').innerText = initPerson.fatherName;
     document.getElementById('surnameOutput').innerText = initPerson.surName;
     document.getElementById('genderOutput').innerText = initPerson.gender;
-    console.log(initPerson.gender);
+    document.getElementById('birthDayOutput').innerText = initPerson.birthDate.day;
+    document.getElementById('birthMonthOutput').innerText = initPerson.birthDate.rusMonth();
+    document.getElementById('birthYearOutput').innerText = initPerson.birthDate.year + ' г.р.';
+    document.getElementById('occupationOutput').innerText = initPerson.occupation;
 };
 
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -74,6 +74,66 @@ const personGenerator = {
         }
     }`,
 
+    occupations: `{
+        "count": 20,
+        "limit": 8,
+        "list": {
+            "id_1": "Шахтер",
+            "id_2": "Солдат",
+            "id_3": "Моряк",
+            "id_4": "Коксоочиститель",
+            "id_5": "Стеклодув",
+            "id_6": "Кузнец",
+            "id_7": "Дробильщик",
+            "id_8": "Ароматизаторщик",
+            "id_9": "Инженер",
+            "id_10": "Оператор техподдержки",
+            "id_11": "Кондитер",
+            "id_12": "Повар",
+            "id_13": "Журналист",
+            "id_14": "Художник",
+            "id_15": "Спортсмен",
+            "id_16": "Водитель",
+            "id_17": "Учитель",
+            "id_18": "Менеджер",
+            "id_19": "Техник",
+            "id_20": "Продавец",
+            "id_21": "Предприниматель"
+        }
+    }`,
+
+    months: `{
+        "count": 12,
+        "list": {
+            "id_1": "Январь",
+            "id_2": "Февраль",
+            "id_3": "Март",
+            "id_4": "Апрель",
+            "id_5": "Май",
+            "id_6": "Июнь",
+            "id_7": "Июль",
+            "id_8": "Август",
+            "id_9": "Сентябрь",
+            "id_10": "Октябрь",
+            "id_11": "Ноябрь",
+            "id_12": "Декабрь"
+        },
+        "days": {
+            "id_1": 31,
+            "id_2": 28,
+            "id_3": 31,
+            "id_4": 30,
+            "id_5": 31,
+            "id_6": 30,
+            "id_7": 31,
+            "id_8": 31,
+            "id_9": 30,
+            "id_10": 31,
+            "id_11": 30,
+            "id_12": 31
+        }
+    }`,
+
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
@@ -89,6 +149,19 @@ const personGenerator = {
         return obj.list[prop];
     },
 
+    randomOccupation: function (gender = 'Мужчина', birthYear = 1994) {
+        const obj = JSON.parse(this.occupations);
+        let curYear = 2021;
+        if (curYear - birthYear < 18) return 'Несовершеннолетний';
+        if (gender === 'Женщина') {
+            const prop = `id_${this.randomIntNumber(obj.count, obj.limit)}`;
+            return obj.list[prop];
+        } else {
+            const prop = `id_${this.randomIntNumber(obj.count, 1)}`;
+            return obj.list[prop];
+        }
+    },
+
     randomFirstName: function(gender) {
         if (gender === 'Женщина') return this.randomValue(this.firstNameFemaleJson);
 
@@ -96,8 +169,29 @@ const personGenerator = {
 
     },
 
+    randomBirthDate: function () {
+        let birthDate = {};
+        let obj = JSON.parse(this.months);
+        birthDate.year = this.randomIntNumber(2021, 1903);
+        birthDate.month = this.randomValue(this.months);
 
-    randomSurname: function(gender) {
+              
+        for (let key in obj.list) {
+            if (obj.list[key] === birthDate.month) {
+               birthDate.day = this.randomIntNumber(obj.days[key], 1);
+            }   
+        }
+
+        //Выводит месяц в правильном падеже
+        birthDate.rusMonth = function() {
+            let month = this.month;
+            if (month.endsWith('ь') || month.endsWith('й')) return `${month.slice(0, month.length - 1)}я`;
+            else return `${month}а`
+        };
+        return birthDate;
+    },
+
+        randomSurname: function(gender) {
         if (gender === 'Женщина') return this.randomValue(this.surnameJson) + 'а';
         else return this.randomValue(this.surnameJson);
 
@@ -111,9 +205,17 @@ const personGenerator = {
                 fatherName = fatherName.slice(0, fatherName.length - 1)+'евна'
             } else if ( fatherName.includes('Юрий') || fatherName.includes('Аркадий') ) {
                 fatherName = fatherName.slice(0, fatherName.length - 2)+'ьевна';
-            } else if ( fatherName.includes('Дмитрий') ) { // для Павла проработать, для Михаила
+            } else if ( fatherName.includes('Дмитрий') ) {
                 fatherName = 'Дмитриевна';
-            } else fatherName = fatherName + 'овна'
+            } else if ( fatherName.includes('Павел') ) {
+                fatherName = 'Павловна';
+            } else if ( fatherName.includes('Михаил') ) {
+                fatherName = 'Михайловна';
+            } else if ( fatherName.includes('Никита') ) {
+                fatherName = 'Никитовна';
+            } else {
+                fatherName = fatherName + 'овна'
+            }    
             return fatherName;
         }
         if (gender === 'Мужчина') {
@@ -124,7 +226,15 @@ const personGenerator = {
                 fatherName = fatherName.slice(0, fatherName.length - 2)+'ьевич';
             } else if ( fatherName.includes('Дмитрий') ) {
                 fatherName = 'Дмитриевич';
-            } else fatherName = fatherName + 'ович'
+            } else if ( fatherName.includes('Павел') ) {
+                fatherName = 'Павлович';
+            } else if ( fatherName.includes('Михаил') ) {
+                fatherName = 'Михайлович';
+            } else if ( fatherName.includes('Никита') ) {
+                fatherName = 'Никитович';
+            } else {
+                fatherName = fatherName + 'ович'
+            }    
             return fatherName;
         }
     },
@@ -136,6 +246,8 @@ const personGenerator = {
         this.person.firstName = this.randomFirstName(this.person.gender);
         this.person.fatherName = this.randomFathername(this.person.gender);
         this.person.surName = this.randomSurname(this.person.gender);
+        this.person.birthDate = this.randomBirthDate();
+        this.person.occupation = this.randomOccupation(this.person.gender, this.person.birthYear);
         return this.person;
     }
 };
